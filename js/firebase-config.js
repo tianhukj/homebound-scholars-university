872c9e8ba5ceeb2c001e3384bae042803229ad4d
tianhukj <166341634+tianhukj@users.noreply.github.com>
2025-05-03 14:06:05 +0800

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,12 @@
+// Firebase 配置文件
+const firebaseConfig = {
+  apiKey: "AIzaSyDKEm99q1_0vrVBwY3v52FqS3M1JYK3PA0",
+  authDomain: "homebound-scholars-university.firebaseapp.com",
+  projectId: "homebound-scholars-university",
+  storageBucket: "homebound-scholars-university.firebasestorage.app",
+  messagingSenderId: "330559202195",
+  appId: "1:330559202195:web:d88f8e1a40e460c67efe66"
+};
+
+// 初始化 Firebase
+firebase.initializeApp(firebaseConfig);
